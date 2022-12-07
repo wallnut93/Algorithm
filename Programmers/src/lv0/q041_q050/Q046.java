b0105e0e56145b37f75a768eb1342553c77d76d0
wallnut93 <wallnut93join@gmail.com>
2022-12-07 13:28:05 +0900

    경고메시지 제거를 위한 주석처리

--- Programmers/src/lv0/q041_q050/Q046.java
+++ Programmers/src/lv0/q041_q050/Q046.java
@@ -1,6 +1,6 @@
 package lv0.q041_q050;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Q046 {
 	/*
@@ -8,16 +8,16 @@ public class Q046 {
 	 * 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
 	 */
 	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        int n = sc.nextInt();
-	        int count = 1;
+	        // Scanner sc = new Scanner(System.in);
+	        // int n = sc.nextInt();
+	        // int count = 1;
 	        
-	        while(count <= n) {
-	        	for(int i = 1; i <= count; i++) {
-	        		System.out.print("*");
-	        	}
-	        	System.out.println("");
-	        	count++;
-	        }
+	        // while(count <= n) {
+	        // 	for(int i = 1; i <= count; i++) {
+	        // 		System.out.print("*");
+	        // 	}
+	        // 	System.out.println("");
+	        // 	count++;
+	        // }
 	    }
 }
