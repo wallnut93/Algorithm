5b3adf6b4f958a15eb91489c18c88d420d2d63c1
wallnut93 <wallnut93join@gmail.com>
2024-06-26 15:03:24 +0900

    2024.06.26 Case_1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import lv_0.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Case_1 c1 = new Case_1();
+
+        int[] arr = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
+        String answer = c1.solution("cvsgiorszzzmrpaqpe", arr);
+
+        System.out.println(answer);
+    }
+}
---
+++ src/lv_0/Case_1.java
@@ -0,0 +1,19 @@
+package lv_0;
+
+public class Case_1 {
+    /*
+     * 글자 이어 붙여 문자열 만들기
+     * 
+     * 문자열 my_string과 정수 배열 index_list가 매개변수로 주어집니다. 
+     * my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 
+     * 문자열을 return 하는 solution 함수를 작성해 주세요.
+     */
+
+     public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        for(int index : index_list){
+            answer += my_string.charAt(index);
+        }
+        return answer;
+    }
+}
